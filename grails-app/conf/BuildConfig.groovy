addde963fa390a3cf36608484d20f416f192c9f5
Andrey Volkov <andvolkov@gmail.com>
2014-12-15 20:46:47 +0300

    Set compatible with Grails 2.4  Hibernate 4. Add Readme.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = 'target'
 grails.project.source.level = 1.6
+grails.project.dependency.resolver = "maven" // switched to Maven instead of Ivy
 
 grails.project.dependency.resolution = {
 
@@ -8,12 +9,13 @@ grails.project.dependency.resolution = {
 
 	repositories {
 		grailsCentral()
+		mavenCentral()
 	}
 
 	dependencies {}
 
 	plugins {
-		build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+		build(':release:3.0.1', ':hibernate4:4.3.6.1', ':rest-client-builder:2.0.3') { // updated plugin versions
 			export = false
 		}
 	}
